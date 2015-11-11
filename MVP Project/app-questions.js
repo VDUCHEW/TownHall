3c8e660063cd124e004737ff9a4b6782ed67303a
VDUCHEW <vidiuchiu@gmail.com>
2015-11-10 21:06:03 -0800

    react MVP project

---
+++ MVP Project/app-questions.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    'q': 'What should Vidiu eat for lunch tomorrow?',
+    'a': 'Chipolet',
+    'b': 'Wendys',
+    'c': 'Hand Delievered Tender Greens from Jihoon'
+  },
+  {
+    'q': 'What is his favorite color?',
+    'a': 'Black',
+    'b': 'Blaccc',
+    'c': 'Really Black'
+  }
+];
